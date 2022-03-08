8d94e7d1263b379fa3253586325336e339004152
gwangjaebak <gjbak0109@naver.com>
2022-03-08 09:54:07 +0900

    react#3

---
+++ 1.basics/webpack-practice/ex05/src/App.js
@@ -0,0 +1,10 @@
+import './assets/scss/App.scss';
+
+const App = function() {
+    const App = document.createElement('h1');
+    App.className = 'Header';
+    App.textContent = 'Hello Webpack';
+    return App;
+}
+
+export { App }
